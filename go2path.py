4778e86264dd754149390f7da2167ad80a2f96f7
Kozo Oeda <kozo.oeda@gmail.com>
2018-10-15 09:06:19 +0900

    Initial commit

---
+++ go2path.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+
+import argparse
+import os
+
+
+def register():
+    parser = argparse.ArgumentParser("advanced cd manipulator")
+    parser.add_argument("path", nargs='?', default=os.getcwd(), type=str)
+    parser.add_argument('-l', '--list', help='list all saved paths', action='store_true')
+    parser.add_argument('-j', '--jump', help='jump to a specified path', nargs=1, action='store', metavar=('num'))
+    parser.add_argument('-s', '--save', help='save a current path', action='store_true')
+    parser.add_argument('-d', '--delete', help='delete a specified path', nargs=1, action='store', metavar=('num'))
+    args = parser.parse_args()
+    return args
+
+
+def load_file():
+    pass
+
+
+def manipulation(args):
+    pass
+
+if __name__ == '__main__':
+    args = register()
+    manipulation(args)
